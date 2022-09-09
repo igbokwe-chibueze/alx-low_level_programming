631fa9a64cc9905c566f74afb737ae3e2199c5d4
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-09-08 19:30:22 -0700

    Task 4-correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 
+ * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
