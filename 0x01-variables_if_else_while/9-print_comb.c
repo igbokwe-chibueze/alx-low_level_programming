a97cdee578a1d3b5067c5b0ec687dd06e56f95a9
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-09-10 14:20:53 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all  possible combination of single digit numbers.
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			break;
+		}
+												putchar(',');
+		putchar(' ');
+											}
+	putchar('\n');
+
+	return (0);
+}
