5ebaeaa6d13cec2d5857ab59740e56607984d300
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-09-15 13:02:45 -0700

    Task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,12 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i < 10 && i != 2 && i != 4)
+	while (i < 10)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 
 		i++;
 	}
