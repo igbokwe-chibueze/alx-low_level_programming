7f32d0820d4ffdab5101143e47978f3e5abf9e7d
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-09-20 12:32:22 -0700

    Task 3-correction2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,15 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: the value to process to the stdout.
  */
+
 void _puts(char *str)
 {
-	while (*str > 0)
+	int i = 0;
+	
+	while (*(str + i) != '\0')
 	{
-		_putchar(*str);
-		str++;
+		putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
