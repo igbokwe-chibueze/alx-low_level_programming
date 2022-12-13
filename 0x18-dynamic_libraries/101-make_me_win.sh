f41bd4cb4b40149df13ed1404f92cd1e1a7ea5fb
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-12-13 02:59:29 -0800

    Task 3-correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
